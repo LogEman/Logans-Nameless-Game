623f40a7238a069934004cb43644710447a66d30
LogEman <teamminers56@gmail.com>
2020-03-21 17:47:35 -0500

    0.3.0 Pre-Alpha
    Portals added to the game! Hint: Press "p" to summon a portal

--- Assets/Nameless Scripts/NamelessPortal.cs
+++ Assets/Nameless Scripts/NamelessPortal.cs
@@ -5,7 +5,15 @@ using UnityEngine.UI;
 using System;
 public class NamelessPortal : MonoBehaviour
 {
-
+    //Reference for do it bool
+    public bool doIt;
+    //Portal being teleported to
+    public GameObject targetPortal;
+    //Portal offsets reference
+    public Vector3 summonPortalOffset;
+    public Vector3 portalOffset;
+    //Reference to portal name string
+    public string portalName;
     //Reference for Mr.Nameless cube's rigidbody
     public Rigidbody mrNamelessCube;
     //Reference for gettingchannel bool
@@ -19,7 +27,10 @@ public class NamelessPortal : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //disables portal name input
+        //Initiates the portalOffset Vectors
+        summonPortalOffset = new Vector3(0.0f, 0.0f, 3.0f);
+        portalOffset = new Vector3(0.0f, 0.0f, -3.0f);
+        //Disables portal name input box
         getPortalName.gameObject.SetActive(false);
         //Makes an object for the MrNameLessCube class to get information
         MrNameLessCube mrNameLessCube = new MrNameLessCube();
@@ -28,22 +39,51 @@ public class NamelessPortal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (doIt)
+        {
+            //Detects if the user is done inputting portal name
+            if (Input.GetKeyDown(KeyCode.Return) && !getPortalName.isFocused)
+            {
+                doIt = false;
+                //Stores the portal name  
+                portalName = getPortalName.text;
+                //Teleports Mr.NamelessCube to the portal selected with offset
+                targetPortal = GameObject.Find(portalName);
+                mrNamelessCube.position = targetPortal.transform.position + portalOffset;
+                //Hides portal name input box
+                getPortalName.gameObject.SetActive(false);
+            }
+        }
         if (Input.GetKeyDown(portalKey))
         {
             //Enables text input field for portal channel
             getPortalName.gameObject.SetActive(true);
             gettingName = true;
         }
-            //checks if user is done with making name
+        //checks if user is done with making name
         if (gettingName && Input.GetKeyDown(KeyCode.Return) && !getPortalName.isFocused)
         {
             //Makes a portal and sets its and then hides input field
-            Instantiate(rigidPortal, mrNamelessCube.position, Quaternion.identity);
-            rigidPortal.name = getPortalName.text;
+            Instantiate(rigidPortal, mrNamelessCube.position + summonPortalOffset, Quaternion.identity);
+            GameObject.FindGameObjectWithTag("isUnactivePortal").name = getPortalName.text;
+            GameObject.FindGameObjectWithTag("isUnactivePortal").tag = "isActivePortal";
             getPortalName.gameObject.SetActive(false);
             gettingName = false;
-            }
         }
-        
     }
+    //Detects for collisions and gets information on the collision
+    public void OnCollisionEnter(Collision portalEntering)
+    {
+
+        //Tests if the collider is a portal
+        if (portalEntering.collider.tag == "isActivePortal")
+        {
+            doIt = true;
+            //Enables input field for portal name
+            getPortalName.gameObject.SetActive(true);        
+        }
+    }
+}
+        
+   
 
